d4be08ce84e5627e77e29216c1aab4d45b2a51c9
yuhonma <c0a2213006@edu.teu.ac.jp>
2024-08-18 01:01:29 +0900

    #10 トップ画面に仮でキャッチフレーズを書きました

---
+++ LP/app.js
@@ -0,0 +1,15 @@
+$(function(){
+    // トップ画面背景色変更
+    // title01
+    setTimeout(function(){
+      $('.fv-title01').css('background-position', '0 0');
+    }, 800 );
+    // title02
+    setTimeout(function(){
+      $('.fv-title02').css('background-position', '0 0');
+    }, 1200 );
+    // title03
+    setTimeout(function(){
+      $('.fv-title03').css('background-position', '0 0');
+    }, 2000 );
+});
